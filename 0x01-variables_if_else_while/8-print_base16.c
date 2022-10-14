3ed0869632b11ed7f63c4a580353220c39993d12
BobbySeg <ayomidegroove006@gmail.com>
2022-10-14 14:24:09 -0700

    base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,87 @@
+nclude <stdio.h>
+
+
+
+
+
+
+
+/**
+ *
+ *  * main - program that prints the lowercase alphabet in reverse.
+ *
+ *   * You can only use the putchar function
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
+
+
+
+int main(void)
+
+
+
+{
+
+
+
+		int n = 48;
+
+
+
+			int l = 'a';
+
+
+
+				while (n <= 57)
+
+
+
+						{
+
+
+
+									putchar(n);
+
+
+
+											n += 1;
+
+
+
+												}
+
+
+
+					while (l <= 'f')
+
+
+
+							{
+
+
+
+										putchar(l);
+
+
+
+												l++;
+
+
+
+													}
+
+
+
+						putchar('\n');
+
+
+
+							return (0);
+
+
+
+}
