ad3fee70bd2cb0996146c39ed54ab0266156c9a7
BobbySeg <113614689+BobbySeg@users.noreply.github.com>
2022-10-14 22:24:32 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,87 +1,44 @@
-nclude <stdio.h>
-
-
-
-
+#include <stdio.h>
 
 
 
 /**
- *
- *  * main - program that prints the lowercase alphabet in reverse.
- *
- *   * You can only use the putchar function
- *
- *    * Return: 0
- *
- *     */
-
-
-
+ * main - program that prints the lowercase alphabet in reverse.
+ * You can only use the putchar function
+ * Return: 0
+ */
 
 
 int main(void)
 
-
-
 {
 
+	int n = 48;
 
+	int l = 'a';
 
-		int n = 48;
+	while (n <= 57)
 
+	{
 
+		putchar(n);
 
-			int l = 'a';
+		n += 1;
 
+	}
 
+	while (l <= 'f')
 
-				while (n <= 57)
+	{
 
+		putchar(l);
 
+		l++;
 
-						{
+	}
 
+	putchar('\n');
 
-
-									putchar(n);
-
-
-
-											n += 1;
-
-
-
-												}
-
-
-
-					while (l <= 'f')
-
-
-
-							{
-
-
-
-										putchar(l);
-
-
-
-												l++;
-
-
-
-													}
-
-
-
-						putchar('\n');
-
-
-
-							return (0);
-
-
+	return (0);
 
 }
