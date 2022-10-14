62934f616acdbbbb538750c73b45af12c946672f
BobbySeg <113614689+BobbySeg@users.noreply.github.com>
2022-10-14 22:14:30 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,51 +1,26 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - program that prints the alphabet in lowercase, followed by a new line
- *
- *   * You can only use the putchar
- *
- *    * Return: 0
- *
- *     */
-
-
-
-
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
+ */
 
 
 
 int main(void)
 
-
-
 {
 
+	int n;
 
+	while (n < 10)
 
-		int n;
+	{
 
-
-
-			while (n < 10)
-
-
-
-					{
-
-
-
-								printf("%d", n);
-
-										n += 1;
-
-											}
-
-				printf("\n");
-
-					return (0);
-
+		printf("%d", n);
+		n += 1;
+	}
+	printf("\n");
+	return (0);
 }
